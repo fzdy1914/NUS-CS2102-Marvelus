0b874ef04edcfdf0a4df3efd2e7912e248e7ab9e
WANG CHAO <wangchao@seagroup.com>
2019-05-27 16:41:04 +0800

    Finalize nav bar

--- frontend/src/components/EventList.vue
+++ frontend/src/components/EventList.vue
@@ -5,44 +5,27 @@
         <h1>Welcome to Event Center !</h1>
       </div>
       <nav class="navbar navbar-default">
-        <div>
-          <a class="navbar-brand" href="#">All</a>
-        </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Link</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="nav navbar-nav navbar-left">
+            <li :class="{ active: !channelId }"><a @click="goChannel(null)">All</a></li>
+            <li v-for="channel in channels.slice(0, 4)" :key="channel.id" :class="{ active: channelId === channel.id }">
+              <a @click="goChannel(channel.id)">{{ channel.name }}</a>
+            </li>
+            <li class="dropdown" :class="{ active: activeDropdown }">
+              <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ dropdownText }}<span class="caret"/></a>
               <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="#">Separated link</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="#">One more separated link</a></li>
+                <li v-for="channel in channels.slice(4)" :key="channel.id" :class="{ active: channelId === channel.id }">
+                  <a @click="goChannel(channel.id)">{{ channel.name }}</a>
+                </li>
               </ul>
             </li>
           </ul>
-          <form class="nav navbar-form navbar-left">
-            <div class="form-group">
-              <input type="text" class="form-control" placeholder="Search">
-            </div>
-            <button type="submit" class="btn btn-default">Submit</button>
-          </form>
           <form class="navbar-right">
             <DatePicker ref="datePicker" v-on:updateSinceDate="updateSinceDate" v-on:updateUntilDate="updateUntilDate"/>
           </form>
         </div><!-- /.navbar-collapse -->
       </nav>
-      <ul class="nav nav-pills">
-        <li :class="{ active: !channelId }"><a @click="goChannel(null)">All</a></li>
-        <li v-for="channel in channels" :key="channel.id" :class="{ active: channelId === channel.id }">
-          <a @click="goChannel(channel.id)">{{ channel.name }}</a>
-        </li>
-      </ul>
       <table class="table table-bordered table-hover">
         <thead>
           <tr>
@@ -251,6 +234,26 @@ export default {
     },
     indexArray: function () {
       return this.$util.generateArray(this.startPage, this.endPage)
+    },
+    dropdownText: function () {
+      if (this.channelId) {
+        for (let channel of this.channels.slice(4)) {
+          if (this.channelId === channel.id) {
+            return channel.name
+          }
+        }
+      }
+      return 'More Channels'
+    },
+    activeDropdown: function () {
+      if (this.channelId) {
+        for (let channel of this.channels.slice(4)) {
+          if (this.channelId === channel.id) {
+            return true
+          }
+        }
+      }
+      return false
     }
   }
 }
@@ -275,5 +278,8 @@ export default {
   }
   .navbar-right {
     vertical-align: center;
+  }
+  .navbar-collapse {
+    padding-left: 0px;
   }
 </style>
