e06d105fe20e0b7863743760288fa1971b44180a
WANG CHAO <wangchao@seagroup.com>
2019-05-28 17:22:16 +0800

    Clean up codebase

--- EntryTask/search2.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.decorators import csrf
-
-
-def search_post(request):
-    ctx = {}
-    if request.POST:
-        ctx['rlt'] = request.POST['out']
-    return render(request, "post.html", ctx)
--- EntryTask/settings.py
+++ EntryTask/settings.py
@@ -65,7 +65,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'TestModel',
     'EventCenter',
 ]
 
