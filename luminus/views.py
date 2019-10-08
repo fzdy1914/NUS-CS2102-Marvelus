47e1ddc7bfa64672501013121997e1b767e53b7a
WANG CHAO <1229983126@qq.com>
2019-10-08 14:28:02 +0800

    Add sql_helper

--- luminus/views.py
+++ luminus/views.py
@@ -1,19 +1,9 @@
 from django.http import HttpResponse
-from django.db import transaction
-from django.db import connections
+from . import sql_helper
 
 
 def template(request):
-    # cursor = connections['default'].cursor()
-    # cursor.execute('select * from channel_tab')
-    # row = cursor.fetchone()  # 只读取一行
-
-    # cursor.execute('select * from channel_tab WHERE name=%s', [row[1]])
-    # rows = cursor.fetchall()  # 读取所有行
-    #
-
-    cursor = connections['luminus'].cursor()
-    cursor.execute('select count(*) from Users')
-    print(cursor.fetchone())
-    transaction.commit()
+    list = sql_helper.fetchall_to_dict('select * from Users')
+    print(list)
+    print(list[0]['uname'])
     return HttpResponse('EXECUTED', status=200)
