66a722d5e31674cb0fa7ebe6e08c54d83832ebce
WANG CHAO <1229983126@qq.com>
2019-10-11 19:08:29 +0800

    Add API for Retrieving Courses

--- luminus/managers/course_manager.py
+++ luminus/managers/course_manager.py
@@ -4,3 +4,24 @@ from luminus import sql_helper
 def get_course(code):
     return sql_helper.fetchone_to_dict("SELECT * FROM Courses WHERE code = %(code)s", {'code': code})
 
+
+def get_course_by_code(code):
+    return sql_helper.fetchall_to_dict("SELECT * FROM Courses WHERE code = %(code)s", {'code': code})
+
+
+def get_course_by_puname(puname):
+    return sql_helper.fetchall_to_dict("SELECT * FROM Courses c, Teach t "
+                                       "WHERE t.uname = %(puname)s AND c.code = t.code",
+                                       {'puname': puname})
+
+
+def get_course_by_tuname(tuname):
+    return sql_helper.fetchall_to_dict("SELECT * FROM Courses c, Assist a "
+                                       "WHERE a.uname = %(tuname)s AND c.code = a.code",
+                                       {'tuname': tuname})
+
+
+def get_course_by_suname(suname):
+    return sql_helper.fetchall_to_dict("SELECT * FROM Courses c, Enroll e "
+                                       "WHERE e.uname = %(suname)s AND c.code = e.code",
+                                       {'suname': suname})
