a9c31713bdc7d19393772081df1dfb8ddf25338f
WANG CHAO <1229983126@qq.com>
2019-10-11 19:06:18 +0800

    Minor fix

--- luminus/views.py
+++ luminus/views.py
@@ -10,21 +10,26 @@ def template(request):
     print(list[0]['uname'])
     return HttpResponse('EXECUTED', status=200)
 
+
 def get_course(request, code):
     course = course_manager.get_course(code)
     return HttpResponse(str(course), status=200)
+
 
 def get_course_by_code(request, code):
     course = course_manager.get_course_by_code(code)
     return HttpResponse(str(course), status=200)
 
+
 def get_course_by_puname(request, puname):
     course = course_manager.get_course_by_puname(puname)
     return HttpResponse(str(course), status=200)
 
+
 def get_course_by_tuname(request, tuname):
     course = course_manager.get_course_by_tuname(tuname)
     return HttpResponse(str(course), status=200)
+
 
 def get_course_by_suname(request, suname):
     course = course_manager.get_course_by_suname(suname)
